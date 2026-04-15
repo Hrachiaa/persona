08dfdad9182ba4d681712020e24dc8422fdec478
Rach <rach.asasian@protonmail.com>
2026-04-15 10:25:45 +0100

    update DB settings for internal url

--- backend/src/prisma.service.ts
+++ backend/src/prisma.service.ts
@@ -5,7 +5,12 @@ import { PrismaClient } from 'generated/prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
     super({ adapter });
   }
 }
